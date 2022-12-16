e52728ef5c440dd7a3b70be1db1ceaceacd2fd3c
Guilherme <Fireguiquenbr@gmail.com>
2022-12-16 19:51:31 -0300

    Update CSS and JS

---
+++ Page/JS/botinhadotitã.js
@@ -0,0 +1,23 @@
+function menu(){
+    return`
+    <nav> 
+        <ul> 
+            <li><a href="./index.html"><img id="logo" src="./Page/Others/Logo/League of legends.svg"> </a></li> 
+        </ul>
+
+    <ul id="menu"> 
+        <li><a href="./about.html">Sobre mim  </a></li>
+        <span class="square"> </span>
+        <li><a href="./playing.html">Durante o game </a></li>
+        <span class="square"> </span>
+        <li><a href="./mains.html">Mains </a></li>
+    </ul>
+</nav>
+    `
+}
+
+
+
+
+var cabeçalho = document.getElementsByTagName('header')[0];
+cabeçalho.innerHTML = menu()
